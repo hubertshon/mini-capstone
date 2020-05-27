930a36c2dee758c5f0fda2440916e6994982a02d
hubert <huby40@gmail.com>
2020-05-27 14:12:56 -0400

    adds singular product routes methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,6 +5,15 @@ def store_page
   render "store.json.jb"
 end
 
+def product
+  @product = Product.first
+  render "products.json.jb"
+end
+
+def noncoffee
+  @product = Product.find_by(id:9)
+  render "noncoffee.json.jb"
+end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
 namespace :api do
   get "/minicapstone_url" => "products#store_page"
+  get "/product_url" => "products#product"
+  get "noncoffee_products_url" => "products#noncoffee"
 end
 
 
