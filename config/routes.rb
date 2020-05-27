895dc5984a0f1f85fc7aae3a25449bc6baa58b55
hubert <huby40@gmail.com>
2020-05-27 12:55:32 -0400

    adds initial work

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+namespace :api do
+  get "/minicapstone_url" => "products#store_page"
+end
+
+
 end
