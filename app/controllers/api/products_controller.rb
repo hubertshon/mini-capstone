1ece4be24a89590945f9c0d17008860323d411f7
hubert <huby40@gmail.com>
2020-05-27 13:48:45 -0400

    adds Starbucks coffee

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,8 +1,10 @@
 class Api::ProductsController < ApplicationController
 
 def store_page
-  
+  @products = Product.all
   render "store.json.jb"
 end
+
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-product =  Product.new(name: "Blue Bottle Coffee", price: 5, image_url:, "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056", desription: "Delicious blue coffee straight from the source!")
+# product =  Product.new(name: "Blue Bottle Coffee", price: 5, image_url: "https://image.cnbcfm.com/api/v1/image/105989971-1561586961365bbcoffeebeanbags.jpg?v=1561587056", description: "Delicious blue coffee straight from the source!")
+# product.save
+
+
+product =  Product.new(name: "Starbucks Coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/H9JQBJ2W6H0Mg3_xIZe5nJbCZsk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19423562/shutterstock_1391647442.jpg", description: "Sugary coffee drink")
+product.save
