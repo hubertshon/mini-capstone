d98939662cbe6ef7c06ebd9180f952bab48704a4
hubert <huby40@gmail.com>
2020-05-27 13:53:45 -0400

    adds ceramic mug

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,9 @@
 # product.save
 
 
-product =  Product.new(name: "Starbucks Coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/H9JQBJ2W6H0Mg3_xIZe5nJbCZsk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19423562/shutterstock_1391647442.jpg", description: "Sugary coffee drink")
-product.save
+# product =  Product.new(name: "Starbucks Coffee", price: 10, image_url: "https://cdn.vox-cdn.com/thumbor/H9JQBJ2W6H0Mg3_xIZe5nJbCZsk=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19423562/shutterstock_1391647442.jpg", description: "Sugary coffee drink")
+# product.save
+
+# product =  Product.new(name: "Ceramic Mug", price: 12, image_url: "https://images.squarespace-cdn.com/content/v1/5563ff5ae4b02c9d26fbff64/1527733520829-I1YD3OYSD3J5RRHPRPVY/ke17ZwdGBToddI8pDm48kO13kjY42Rx8PDuIzbsUGx97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UdW2N0leaE2nInJg7KpfqMS1mKwPiDa5VuS53xofCarKLCwLgRJzBxPM8v-1l46jLA/IMG_0916.JPG?format=1000w", description: "Decorative mug for your coffee")
+# product.save
+
