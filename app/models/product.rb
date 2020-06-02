09e937a412ea2056a4ecfce462a465e0456f8d8d
hubert <huby40@gmail.com>
2020-06-02 18:31:54 -0400

    adds model methods: discount,tax,total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      false
+    end
+  end
+
+  def tax
+    return (price * 0.090).round(2)
+  end
+
+  def total
+    return tax + price
+  end
 end
