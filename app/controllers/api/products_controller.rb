fe32816f52679df58680002fb8ac4f8c292ad00b
hubert <huby40@gmail.com>
2020-06-02 14:08:32 -0400

    adds update,delete, and cors

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,18 +1,41 @@
 class Api::ProductsController < ApplicationController
 
-def store_page
+def index
   @products = Product.all
-  render "store.json.jb"
+  render "index.json.jb"
 end
 
-def product
-  @product = Product.first
+def show
+  @product = Product.find_by(id: params[:id])
+  render "show.json.jb"
+end
+
+def create
+  product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    image_url: params[:image_url],
+    description: params[:description]
+  )
+  product.save
+  render "index.json.jb"
+end
+
+def update
+  @product = Product.find(params[:id])
+  @product.name = params[:name] || @product.name
+  @product.price = params[:price] || @product.price
+  @product.image_url = params[:image_url] || @product.image_url
+  @product.description = params[:description] || @product.description
+
+  @product.save
   render "products.json.jb"
 end
 
-def noncoffee
-  @product = Product.find_by(id:9)
-  render "noncoffee.json.jb"
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  render json: "Item Deleted"
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
 
 namespace :api do
-  get "/minicapstone_url" => "products#store_page"
-  get "/product_url" => "products#product"
-  get "noncoffee_products_url" => "products#noncoffee"
+  get "/products" => "products#index"
+  get "products/:id" => "products#show" 
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
+
+  
 end
 
 
